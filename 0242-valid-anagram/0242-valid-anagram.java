6fdb51a6b622822df437e77ac8e14a53ca8db876
AryaKhamayacha <khamayacha.arya@gmail.com>
2026-06-07 10:55:41 +0530

    Time: 15 ms (24.97%), Space: 46.5 MB (23.62%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> hashmap = new HashMap<>();
+
+        if (s.length() != t.length()) return false;
+
+        // Step 1: Count frequency from s
+        for (char c : s.toCharArray()) {
+            hashmap.put(c, hashmap.getOrDefault(c, 0) + 1);
+        }
+
+        // Step 2: Reduce frequency using t
+        for (char c : t.toCharArray()) {
+            if (!hashmap.containsKey(c)) return false;
+
+            hashmap.put(c, hashmap.get(c) - 1);
+
+            if (hashmap.get(c) < 0) return false;
+        }
+
+        return true;
+    }
+}
