e56d12e584d8b4ac9e0ab366c70c1ab6052194bd
AryaKhamayacha <khamayacha.arya@gmail.com>
2026-06-07 11:35:17 +0530

    Time: 5 ms (62.55%), Space: 44.5 MB (60.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,40 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         // HashMap<Character, Integer> hashmap = new HashMap<>();
+
+//         // if (s.length() != t.length()) return false;
+
+//         // // Step 1: Count frequency from s
+//         // for (char c : s.toCharArray()) {
+//         //     hashmap.put(c, hashmap.getOrDefault(c, 0) + 1);
+//         // }
+
+//         // // Step 2: Reduce frequency using t
+//         // for (char c : t.toCharArray()) {
+//         //     if (!hashmap.containsKey(c)) return false;
+
+//         //     hashmap.put(c, hashmap.get(c) - 1);
+
+//         //     if (hashmap.get(c) < 0) return false;
+//         // }
+
+//         // return true;
+        
+//     }
+// }
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> hashmap = new HashMap<>();
-
         if (s.length() != t.length()) return false;
 
-        // Step 1: Count frequency from s
-        for (char c : s.toCharArray()) {
-            hashmap.put(c, hashmap.getOrDefault(c, 0) + 1);
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
 
-        // Step 2: Reduce frequency using t
-        for (char c : t.toCharArray()) {
-            if (!hashmap.containsKey(c)) return false;
-
-            hashmap.put(c, hashmap.get(c) - 1);
-
-            if (hashmap.get(c) < 0) return false;
+        for (int val : count) {
+            if (val != 0) return false;
         }
 
         return true;
